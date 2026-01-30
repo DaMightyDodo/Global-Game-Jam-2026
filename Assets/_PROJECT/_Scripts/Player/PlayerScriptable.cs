0ad12c1b27f09511c6c18135bcdb0cf706f57e59
DaMightyDodo <73170977+DaMightyDodo@users.noreply.github.com>
2026-01-31 00:07:46 +0700

    camerawork33

--- Assets/_PROJECT/_Scripts/Player/PlayerScriptable.cs
+++ Assets/_PROJECT/_Scripts/Player/PlayerScriptable.cs
@@ -5,7 +5,10 @@ public class PlayerScriptable : ScriptableObject
 {
     [Header("Movement")]
     public float walkSpeed = 5f;
+    public float turnSpeed = 5f;
+    [Header("Jump")]
     public float gravity = 9.81f;
+    public float gravityMultiplier = 3.3f;
     public float jumpHeight = 2f;
     [Header("Jump Buffer")]
     public float jumpBufferTime = 0.22f;
