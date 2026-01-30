d60fff1bac47701fe745a368ea9ddd9a94bae2ea
DaMightyDodo <73170977+DaMightyDodo@users.noreply.github.com>
2026-01-30 21:56:00 +0700

    simplejump

--- Assets/_PROJECT/_Scripts/PlayerController.cs
+++ Assets/_PROJECT/_Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,11 +9,14 @@ public class PlayerController : MonoBehaviour
 
     [Header("Movement")]
     [SerializeField] private float _walkSpeed = 5f;
-
+    [SerializeField] private float _gravity = 9.81f;
+    [SerializeField] private float _jumpHeight = 2f;
+    [SerializeField] private float _sprintSpeed = 5f;
+    private float _verticalVelocity;
     [Header("Input")]
     private float _moveInput;
     private float _turnInput;
-
+    private bool _jumpInput;
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
@@ -32,12 +36,40 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 move = new Vector3(_turnInput, 0, _moveInput);
         move *= _walkSpeed;
+        move.y = VerticalForceCalculation();
         _controller.Move(move * Time.deltaTime);
+    }
+
+    private float VerticalForceCalculation()
+    {
+        if (_controller.isGrounded)
+        {
+            _verticalVelocity = -1f;
+
+            if (_jumpInput)
+            {
+                _verticalVelocity = Mathf.Sqrt(_jumpHeight * _gravity * 2);
+                _jumpInput = false;
+            }
+        }
+        else
+        {
+            _verticalVelocity -= _gravity * Time.deltaTime;
+        }
+        return _verticalVelocity;
     }
     private void OnMove(InputValue value)
     {
         Vector2 input = value.Get<Vector2>();
         _turnInput = input.x;
         _moveInput = input.y;
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed && _controller.isGrounded)
+        {
+            _jumpInput = true;
+        }
     }
 }
