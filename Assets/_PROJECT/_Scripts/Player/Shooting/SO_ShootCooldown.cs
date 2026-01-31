557b64700fc06b01937b2400ee11d5c8b72f4c27
toilatruongphuc <phuc.tt@st.buv.edu.vn>
2026-01-31 18:22:50 +0700

    Finished the shooting(base) and bullet

---
+++ Assets/_PROJECT/_Scripts/Player/Shooting/SO_ShootCooldown.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_ShootCooldown", menuName = "Scriptable Objects/SO_ShootCooldown")]
+public class SO_ShootCooldown : ScriptableObject
+{
+    public float cooldown = 1.0f;
+}
