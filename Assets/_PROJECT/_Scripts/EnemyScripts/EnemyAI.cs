6e848d9f74f529a8dc1fb78b0dadd9ca532f2807
HoangAnhTuna <88965746+HoangAnhTuna@users.noreply.github.com>
2026-01-31 15:57:32 +0700

    Fixed Enemies
    Enemies' destination now fixed to the x axis

--- Assets/_PROJECT/_Scripts/EnemyScripts/EnemyAI.cs
+++ Assets/_PROJECT/_Scripts/EnemyScripts/EnemyAI.cs
@@ -73,7 +73,7 @@ abstract public class EnemyAI : MonoBehaviour
         //Calculate random point in range
         float randomZ = Random.Range(-enemyStats.walkPointRange, enemyStats.walkPointRange);
         //float randomX = Random.Range(-enemyStats.walkPointRange, enemyStats.walkPointRange);
-        walkPoint = new Vector3(groundCheck.position.x, groundCheck.position.y, groundCheck.position.z + randomZ);
+        walkPoint = new Vector3(0, groundCheck.position.y, groundCheck.position.z + randomZ);
         if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
             walkPointSet = true;
     }
