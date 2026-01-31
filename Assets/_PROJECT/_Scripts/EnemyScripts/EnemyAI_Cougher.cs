e84862af8e94a769e20c6becc1add9a3808d55a3
HoangAnhTuna <88965746+HoangAnhTuna@users.noreply.github.com>
2026-01-31 13:40:38 +0700

    Enemy Shooting Behavior

--- Assets/_PROJECT/_Scripts/EnemyScripts/EnemyAI_Cougher.cs
+++ Assets/_PROJECT/_Scripts/EnemyScripts/EnemyAI_Cougher.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class EnemyAI_Cougher : EnemyAI
 {
+    public GameObject germ;
     public override void AttackAction()
     {
         // Implement the specific attack action for the Cougher enemy
+        Rigidbody rb = Instantiate(germ, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+
         Debug.Log("Cougher Enemy Attacks with a cough!");
-        // Here you can add the logic for the cough attack, such as dealing damage or applying effects to the player
     }
 }
