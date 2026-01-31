f8275d0ef8c46d331eabebcb4d18d0f428944d91
toilatruongphuc <phuc.tt@st.buv.edu.vn>
2026-01-31 14:00:11 +0700

    Can shoot now

--- Assets/_PROJECT/_Scripts/Player/Shooting/Shootingbase.cs
+++ Assets/_PROJECT/_Scripts/Player/Shooting/Shootingbase.cs
@@ -3,15 +3,41 @@ using UnityEngine.InputSystem;
 
 public class Shootingbase : MonoBehaviour
 {
+    [Header("ScriptableObjects")]
+    [SerializeField] private SO_BulletStatistics so_bulletStatistics;
+    
     [SerializeField] private Camera mainCam;
     [SerializeField] private Transform rotatePoint;
-
+    
     private Vector3 mousePos;
 
+    [Header("New Input System")] private InputSystem_Actions actions;
+
+    [Header("Relating to Bullets (mask)")]
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firepoint;
+    void Awake()
+    {
+        actions = new InputSystem_Actions();
+    }
+
+    void OnEnable()
+    {
+        actions.Player.Enable();
+        actions.Player.Attack.performed += OnLeftClick;
+        actions.Player.Attack.canceled += OnLeftClick;
+    }
+
+    void OnDisable()
+    {
+        actions.Player.Disable();
+        actions.Player.Attack.performed -= OnLeftClick;
+        actions.Player.Attack.canceled -= OnLeftClick;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+    
     }
 
     // Update is called once per frame
@@ -19,7 +45,12 @@ public class Shootingbase : MonoBehaviour
     {
         // mousePos = mainCam.ScreenToViewportPoint(Mouse.current.position.ReadValue());
         // Debug.Log(mousePos);
+        MousePosition_and_Rotation();
 
+    }
+
+    private void MousePosition_and_Rotation()
+    {
         // 1. Get mouse position
         Vector2 screenPos = Mouse.current.position.ReadValue();
         Ray ray = mainCam.ScreenPointToRay(screenPos);
@@ -42,6 +73,22 @@ public class Shootingbase : MonoBehaviour
             {
                 rotatePoint.rotation = Quaternion.LookRotation(direction);
             }
+        }    
+    }
+    private void OnLeftClick(InputAction.CallbackContext context)
+    {
+        //mousePos = context.ReadValue<Vector3>();
+        
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, rotatePoint.rotation);
+        
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        
+        //FAILSAFE
+        if (rb != null)
+        {
+            // Vector3 direction = new Vector3(rotatePoint.forward.x, rotatePoint.forward.y, rotatePoint.forward.z);
+            
+            rb.linearVelocity = rotatePoint.forward * so_bulletStatistics.speed;
         }
     }
 }
